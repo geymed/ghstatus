feca333609e6b7cccaa47aacab5ae64a15e3c82d
giladf <gilad.foyer@gmail.com>
2015-11-12 00:27:10 +0200

    gh status initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#GitHub Status app
+A simple web page showing GitHub current system status and latest activity messages.
+
+##Installation
+Execute `npm install`
+
+##Running
+1. Run `npm start`
+2. Open `http://localhost:3000` in your browser.
+
+##Testing
+No tests were written yet.
+However, live github status can be replaced with a mock by changing **"test"** value tp **"true"** in `api/config.json`
+
+
+
+
---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var ghstatus = require ('./api')
+var exphbs  = require('express-handlebars');
+var moment = require('moment');
+var Q = require('q');
+
+var app = express();
+var hbs = exphbs.create({
+	helpers: {
+		datify: function (dateStr) {
+			return moment(new Date(dateStr)).format("DD/MM/YY HH:mm");
+		}
+	}
+});
+
+// Register `hbs.engine` with the Express app.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/',function (req,res) {
+	Q.all([ghstatus.status(),ghstatus.messages()]).done(
+	 function(results) {
+	 	res.render('home',{
+	 		status:results[0],
+	 		messages:results[1]
+	 	});
+	 });
+	
+});
+
+//start server on port 3000
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('GitHub Status app listening at http://%s:%s', host, port);
+  });
